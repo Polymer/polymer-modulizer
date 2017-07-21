2870be2e254a0793f704e75c23f6512897062ffc
Peter Burns <rictic@google.com>
2017-07-20 21:23:24 -0700

    Can discover the Polymer namespace, it does not need to be a default.

--- src/convert-package.ts
+++ src/convert-package.ts
@@ -73,7 +73,7 @@ export function configureAnalyzer(options: ConvertPackageOptions) {
 export function configureConverter(
     analysis: Analysis, options: ConvertPackageOptions) {
   return new AnalysisConverter(analysis, {
-    rootNamespaces: options.rootNamespaces || ['Polymer'],
+    rootNamespaces: options.rootNamespaces,
     excludes: options.excludes ||
         [
           'lib/elements/dom-module.html',
